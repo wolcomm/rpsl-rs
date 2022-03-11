9b3cc263f00f6893742ff7c8b01b709273d1db53
Ben Maddison <benm@workonline.africa>
2022-03-11 21:46:15 +0200

    remove defunct doctest example

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,6 @@
 //!  A parser and syntax tree implementation for the Routing Policy
 //!  Specification Language (RPSL) defined in [RFC2622] and [RFC4012].
 //!
-//! # Example
-//!
-//! ``` rust
-//! use std::error::Error;
-//! use rpsl::{
-//!     expr::MpFilterExpr,
-//!     names::AutNum,
-//!     subst::Substitute,
-//! };
-//!
-//! fn main() -> Result<(), Box<dyn Error>> {
-//!     let peeras = "AS65000".parse::<AutNum>()?;
-//!     let filter = "PeerAS:AS-FOO AND {0.0.0.0/0^8-24, ::/0^16-48}"
-//!         .parse::<MpFilterExpr>()?
-//!         .substitute(&peeras)?;
-//!
-//!     assert_eq!(filter.to_string(), "AS65000:AS-FOO AND {0.0.0.0/0^8-24, ::/0^16-48}");
-//!     Ok(())
-//! }
-//! ```
 //! [RFC2622]: https://datatracker.ietf.org/doc/html/rfc2622
 //! [RFC4012]: https://datatracker.ietf.org/doc/html/rfc4012
 #![doc(html_root_url = "https://docs.rs/rpsl/0.1.0-alpha.1")]
